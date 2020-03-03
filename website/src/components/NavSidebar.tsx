0dd617750da01c3b630342179daee2be5379dfa7
Paul Shen <paulshen0@gmail.com>
2020-03-02 21:46:48 -0800

    [website] sidebar styling

--- website/src/components/NavSidebar.tsx
+++ website/src/components/NavSidebar.tsx
@@ -1,27 +1,26 @@
 import { Link } from "gatsby"
 import * as React from "react"
-
 import styles from "./NavSidebar.module.css"
+
+function NavLink({ to, children }: { to: string; children: React.ReactNode }) {
+  return (
+    <li>
+      <Link to={to} activeClassName={styles.navListActiveLink}>
+        {children}
+      </Link>
+    </li>
+  )
+}
 
 export default function NavSidebar() {
   return (
     <nav className={styles.nav}>
       <ul className={styles.navList}>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/layer">Layer</Link>
-        </li>
-        <li>
-          <Link to="/dialog">Dialog</Link>
-        </li>
-        <li>
-          <Link to="/popperlayer">PopperLayer</Link>
-        </li>
-        <li>
-          <Link to="/tooltip">Tooltip</Link>
-        </li>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/layer">Layer</NavLink>
+        <NavLink to="/dialog">Dialog</NavLink>
+        <NavLink to="/popperlayer">PopperLayer</NavLink>
+        <NavLink to="/tooltip">Tooltip</NavLink>
       </ul>
     </nav>
   )
