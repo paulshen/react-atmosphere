5302273cd265471f49f5b3f5953c9adc619c23bf
Paul Shen <paulshen0@gmail.com>
2020-03-04 13:07:56 -0800

    TooltipConfigContext

--- packages/millefeuille/Tooltip.tsx
+++ packages/millefeuille/Tooltip.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import PopperLayer from "./PopperLayer";
-import { Placement, State } from "@popperjs/core";
+import { Placement, State, Options } from "@popperjs/core";
 
 type TooltipRenderProps = {
   onMouseEnter: () => void;
@@ -12,33 +12,41 @@ function defaultRenderTooltip(text: React.ReactNode) {
   return text;
 }
 
-export const TooltipRenderContext = React.createContext<
-  (
+export const TooltipConfigContext = React.createContext<{
+  renderTooltip: (
     text: React.ReactNode,
     props: { popperState: State | undefined }
-  ) => React.ReactNode
->(defaultRenderTooltip);
+  ) => React.ReactNode;
+  options?: Partial<Options>;
+}>({
+  renderTooltip: defaultRenderTooltip
+});
 
 type TooltipProps = {
   children: (tooltipProps: TooltipRenderProps) => React.ReactNode;
   text: React.ReactNode;
-  placement?: Placement;
+  options?: Partial<Options>;
 };
 
 export default function Tooltip({
   children,
   text,
-  placement = "top"
+  options: optionsProp
 }: TooltipProps) {
   const domRef = React.useRef<Element>(null);
-  const renderTooltip = React.useContext(TooltipRenderContext);
+  const { renderTooltip, options: optionsContext } = React.useContext(
+    TooltipConfigContext
+  );
   const [showTooltip, setShowTooltip] = React.useState(false);
 
   const render = React.useCallback(
     popperProps => renderTooltip(text, popperProps),
     [text, renderTooltip]
   );
-  const options = React.useMemo(() => ({ placement }), [placement]);
+  const options = React.useMemo(() => ({ ...optionsContext, ...optionsProp }), [
+    optionsContext,
+    optionsProp
+  ]);
 
   return (
     <>
