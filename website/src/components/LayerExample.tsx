8d753096e062d4a5c59ec97672c9632886f305e7
Paul Shen <paulshen0@gmail.com>
2020-03-04 10:49:07 -0800

    [website] Updated LayerExample

--- website/src/components/LayerExample.tsx
+++ website/src/components/LayerExample.tsx
@@ -1,28 +1,50 @@
-import * as React from "react"
 import { Layer } from "millefeuille"
+import * as React from "react"
+import styles from "./LayerExample.module.css"
+
+let nextLayerKey = 1
 
 export default function LayerExample() {
+  const [layers, setLayers] = React.useState<
+    Array<{ x: number; y: number; rotate: number; key: number }>
+  >([])
   return (
-    <Layer
-      render={() => (
-        <div
-          style={{
-            position: "absolute",
-            backgroundColor: "#7e4efc",
-            color: "#ffffff",
-            fontSize: "12px",
-            padding: "24px",
-            bottom: "128px",
-            right: "128px",
-            transform: "rotate(-4deg)",
-          }}
-        >
-          <div>Hello!</div>
-          <div>
-            {"This is a layer component rendered inside the <LayerContainer>"}
-          </div>
-        </div>
-      )}
-    />
+    <>
+      <button
+        onClick={() =>
+          setLayers(layers => [
+            ...layers,
+            {
+              x: Math.random(),
+              y: Math.random(),
+              rotate: Math.random(),
+              key: nextLayerKey++,
+            },
+          ])
+        }
+      >
+        Add Layer
+      </button>
+      {layers.map(({ x, y, rotate, key }) => (
+        <Layer
+          render={() => (
+            <div
+              className={styles.layer}
+              style={{
+                top: `${y * 90}vh`,
+                left: `${x * 90}vw`,
+                transform: `rotate(${rotate * 16 - 8}deg)`,
+              }}
+              onClick={() =>
+                setLayers(layers => layers.filter(layer => layer.key !== key))
+              }
+            >
+              ✕
+            </div>
+          )}
+          key={key}
+        />
+      ))}
+    </>
   )
 }
