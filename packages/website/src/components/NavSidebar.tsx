1174f7b8ef350c20177e80186ea69a48e20875e6
Paul Shen <paulshen0@gmail.com>
2020-03-11 17:36:08 -0700

    [website] Accessibility page

--- packages/website/src/components/NavSidebar.tsx
+++ packages/website/src/components/NavSidebar.tsx
@@ -29,6 +29,7 @@ export default function NavSidebar() {
       <h4>Topics</h4>
       <ul className={styles.navList}>
         <NavLink to="/imperative-api">Imperative API</NavLink>
+        <NavLink to="/accessibility">Accessibility</NavLink>
         <NavLink to="/packages">Packages</NavLink>
         <NavLink to="/performance">Performance</NavLink>
       </ul>
